17622093b870d7e4eb6278bcf7f7c4eac6109e18
Karolina Strzelecka <kstrzelecka0594@gmail.com>
2017-12-06 16:40:50 +0100

    Change .js file

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,12 @@ var n ;
 function drawTree(n) {
 	
 	for (i=0;i<=n;i++) {
-		var star;
-			for(j=0; j<=i; j++) {
-				star = star += '*';
-			}
+		var star=" ";
+			for(var j = 0; j < (n - i); j++) {
+      			star += " ";}
+     			 for(var k = 0; k <= i*2; k++) {
+        			star+= "*";}
+    
 		console.log(star);
 		}
 }
